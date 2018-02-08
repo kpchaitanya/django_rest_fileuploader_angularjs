61c22f25ba32758338388c218abca6b42a1cb393
Omar Ezz Ed-Dien <om4rezz@linux.ninja.box>
2018-02-08 02:24:52 +0200

    add angular filesModelDirective and fix transformRequest

--- fileupload_frontend/static/js/app.js
+++ fileupload_frontend/static/js/app.js
@@ -8,4 +8,13 @@ myApp.controller('MainCtrl', function($scope, Files){
     console.log('In main control');
 
     $scope.files = Files.query();
+
+    $scope.newFile = {};
+
+    $scope.uploadFile = function(){
+        console.log('w00t w00t! Upload file invoked!');
+        Files.save($scope.newFile);
+    }
 });
+
+myApp.directive('filesModel', filesModelDirective);
--- fileupload_frontend/static/js/files.rest.js
+++ fileupload_frontend/static/js/files.rest.js
@@ -1,3 +1,31 @@
+function transformFileRequest(data) {
+    if (data === undefined)
+        return data;
+
+    var fd = new FormData();
+    angular.forEach(data, function (value, key) {
+        if (value instanceof FileList) {
+            if (value.length == 1) {
+                fd.append(key, value[0]);
+            } else {
+                angular.forEach(value, function (file, index) {
+                    fd.append(key + '_' + index, file);
+                });
+            }
+        } else {
+            fd.append(key, value);
+        }
+    });
+
+    return fd;
+}
+
 myApp.factory('Files', function ($resource) {
-    return $resource("/api/files/:pk/", {'pk': '@pk'});
+    return $resource("/api/files/:pk/", {'pk': '@pk'}, {
+        'save': {
+            'method': 'POST',
+            'transformRequest': transformFileRequest,
+            'headers': {'Content-Type': undefined}
+        }
+    });
 });
